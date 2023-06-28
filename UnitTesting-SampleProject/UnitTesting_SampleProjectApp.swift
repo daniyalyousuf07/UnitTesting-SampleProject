9e48d533651c2d32f2e29cd68305c38e2839d313
Daniyal <daniyalyousuf44@gmail.com>
2023-06-28 14:42:49 -0400

    Initial Commit

---
+++ UnitTesting-SampleProject/UnitTesting_SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTesting_SampleProjectApp.swift
+//  UnitTesting-SampleProject
+//
+//  Created by Daniyal Yousuf on 2023-06-28.
+//
+
+import SwiftUI
+
+@main
+struct UnitTesting_SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
